32cca7b67af4d836cd45f69f25b1391626072423
johnSilver7 <inspiredevelopment@hotmail.fr>
2015-02-13 15:23:01 +0100

    Premier Commit du TP2

---
+++ src/main/java/dcll/jbir/App.java
@@ -0,0 +1,13 @@
+package dcll.jbir;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
