6b29feb9d984ba5048ea1fcda83350620902e7d8
johnSilver7 <inspiredevelopment@hotmail.fr>
2015-02-23 11:00:10 +0100

    Premier Commit du TP2

--- src/main/java/SimpleStack.java
+++ src/main/java/SimpleStack.java
@@ -1,5 +1,32 @@
 /**
  * Created by 21105311 on 23/02/2015.
  */
+import java.util.*;
+import java.lang.Object;
+
 public interface SimpleStack {
+    Stack st = new Stack();
+
+    /*Tests if stack is empty*/
+    public boolean isEmpty();
+
+    /*Returns the number of items in the stack*/
+    public int getSize();
+
+    /*Pushes an item onto the top of this Stack
+    null item is allowed
+     */
+    public void push(Stack item);
+
+    /*Looks at the object at the top of this stack without removing it from the stack.*/
+    public Stack peek() throws EmptyStackException;
+
+    /**
+     * Removes the object at the top of this stack and returns that object as the value of this function.
+     * @throws EmptyStackException if this stack is empty.
+     */
+    public Stack pop() throws EmptyStackException;
 }
+
+
+
